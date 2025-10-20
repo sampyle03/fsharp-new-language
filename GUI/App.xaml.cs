6ba0a9af307db5a0ba4bf389c7ae2584da8ed39c
liberty <liblo@LIBERTYSLAPTOP>
2025-10-20 10:50:26 +0100

    added gui WPF

---
+++ GUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Numera.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
