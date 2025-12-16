59df80a7c004219995a19e09da3eec947c027e5d
liberty <136236472+liberty0408@users.noreply.github.com>
2025-12-16 14:57:06 +0000

    plotting

--- NumeraGUI/HelpWindow.xaml.cs
+++ NumeraGUI/HelpWindow.xaml.cs
@@ -1,4 +1,16 @@
-﻿using System.Windows;
+/*
+* HelpWindow.xaml.cs
+* ------------------
+* This file controls the Help window for the Numera application.
+*
+* The Help window is a simple, modal dialog that displays
+* instructions and usage information for the user.
+*
+* It does not interact with the interpreter or application state;
+* it purely provides guidance and can be closed by the user.
+*/
+
+using System.Windows;
 
 namespace NumeraGUI
 {
@@ -6,9 +18,15 @@ namespace NumeraGUI
     {
         public HelpWindow()
         {
+            // Initialise the Help window UI defined in HelpWindow.xaml
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the Close button click.
+        /// Simply closes the help window and returns control
+        /// to the main application window.
+        /// </summary>
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
